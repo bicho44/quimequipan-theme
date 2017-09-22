d76367a9e1036a214be1952ee553bc38c37e2c8c
Federico <admin@imgdigital.com.ar>
2017-09-22 16:16:01 -0300

    cambio de lugar de la imagen otra vez

--- page-imagen-grande.php
+++ page-imagen-grande.php
@@ -12,29 +12,28 @@ get_header(); ?>
 
 	<div id="primary" class="content-area container">
 <div class="row">
-
-		<main id="main" class="site-main col-md-8" role="main">
-			<?php
+<?php
 			while ( have_posts() ) : the_post();
-
+			
 				if (has_post_thumbnail()){
 					the_post_thumbnail('header-cropped');
 				}
-
+				
+			?>
+		<main id="main" class="site-main col-md-8" role="main">
+			<?php
 				get_template_part( 'template-parts/content', 'page-img-grande' );
 
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
 				endif;
-
-			
 			?>
 		</main><!-- #main -->
 		<?php get_sidebar(); ?>
 
 		<?php endwhile; // End of the loop. ?>
-		
+
 		</div>
 	</div><!-- #primary -->
 
