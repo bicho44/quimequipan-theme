17dd41f0e5471dde9c1be2a3a8fee5cb4fc395e4
Federico <admin@imgdigital.com.ar>
2017-09-22 16:00:41 -0300

    pagina de imagen grande

---
+++ page-imagen-grande.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * Template Name: Imagen Grande
+ * Description: Página con la imagen ancha que cubre las dos columnas
+ * 
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Hostería QuimeQuipan
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area container">
+<div class="row">
+
+		<main id="main" class="site-main col-md-8" role="main">
+			<?php
+			while ( have_posts() ) : the_post();
+
+				if (has_post_thumbnail()){
+					the_post_thumbnail('header-cropped');
+				}
+
+				get_template_part( 'template-parts/content', 'page-img-grande' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+		</main><!-- #main -->
+		<?php get_sidebar(); ?>
+		</div>
+	</div><!-- #primary -->
+
+<?php
+get_footer();
